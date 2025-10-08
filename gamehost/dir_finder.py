da831952706663315e830099ca9b1097a7a58720
farrar142 <gksdjf1690@gmail.com>
2025-10-08 21:10:04 +0900

    에러 수정

--- gamehost/dir_finder.py
+++ gamehost/dir_finder.py
@@ -388,17 +388,17 @@ def index() -> rx.Component:
                                             rx.cond(
                                                 game.status == GameStatus.NOTCREATED,
                                                 rx.button(
+                                                    "실행",
+                                                    on_click=lambda: Games.run_game(
+                                                        game.id
+                                                    ),
+                                                ),
+                                                rx.button(
                                                     "생성중",
                                                     on_click=lambda: Games.run_game(
                                                         game.id
                                                     ),
                                                     color_scheme="orange",
-                                                ),
-                                                rx.button(
-                                                    "실행",
-                                                    on_click=lambda: Games.run_game(
-                                                        game.id
-                                                    ),
                                                 ),
                                             ),
                                         ),
